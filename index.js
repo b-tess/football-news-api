cb7002356d384252e0d956aebb5913b1891b9d9f
LogSyn <barbaratessn@gmail.com>
2023-01-27 15:58:55 +0300

    Newspapers added but not working properly yet

--- index.js
+++ index.js
@@ -7,7 +7,48 @@ const cheerio = require('cheerio')
 //Call the express package to initialize it
 const app = express()
 
+//Create an array of newspapers
+const newspapers = [
+    {
+        name: 'thestandard',
+        address: 'https://www.standardmedia.co.ke/sports/category/29/football',
+        base: '',
+    },
+    {
+        name: 'thestar',
+        address: 'https://www.the-star.co.ke/sports/football',
+        base: 'https://www.the-star.co.ke',
+    },
+    {
+        name: 'theguardian',
+        address: 'https://www.theguardian.com/football',
+        base: '',
+    },
+]
+
 const articles = []
+
+//Loop through the newspapers and get all the relevant elements
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+
+            //Give the returned response to cheerio to help with picking out what is needed
+            const $ = cheerio.load(html)
+            $('a:contains("football")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name,
+                })
+            })
+        })
+        .catch((error) => console.log(error))
+})
 
 //Create a generic home path/route
 app.get('/', (req,res) => {
@@ -16,25 +57,28 @@ app.get('/', (req,res) => {
 
 //Create a route for the articles returned by cheerio
 app.get('/footballnews', (req,res) => {
-    axios.get('https://www.theguardian.com/football')
-        .then((response) => {
-            const html = response.data
+
+    res.json(articles)
+
+    // axios.get('https://www.theguardian.com/football')
+    //     .then((response) => {
+    //         const html = response.data
             
-            //Give the returned response to cheerio to help with picking out what is needed
-            const $ = cheerio.load(html)
+    //         //Give the returned response to cheerio to help with picking out what is needed
+    //         const $ = cheerio.load(html)
 
-            $('a:contains("football")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
+    //         $('a:contains("football")', html).each(function () {
+    //             const title = $(this).text()
+    //             const url = $(this).attr('href')
 
-                articles.push({
-                    title,
-                    url,
-                })
-            })
-            res.json(articles)
-        })
-        .catch((error) => console.log(error))
+    //             articles.push({
+    //                 title,
+    //                 url,
+    //             })
+    //         })
+    //         res.json(articles)
+    //     })
+    //     .catch((error) => console.log(error))
 })
 
 //Check if express is listening to the dedicated port.
