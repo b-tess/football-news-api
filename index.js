4a041f64b1f30080f31fd84cede574004751aa9c
LogSyn <barbaratessn@gmail.com>
2023-08-16 10:57:17 +0300

    On page reload behavior changed
    The page now keeps the same number of article objects on page reload
    instead of adding the same articles onto what was initially on the page.
    This was lengthening the number of articles on the page with repetition.
    Similar to clearing the state/page whenever it is reloaded.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const newspapers = [
     },
 ]
 
-const articles = []
+let articles
 
 //Create a generic home path/route
 app.get('/', (req, res) => {
@@ -40,33 +40,39 @@ app.get('/', (req, res) => {
 
 //Create a route for the articles from all the newspapers returned by cheerio
 app.get('/footballnews', async (req, res) => {
-    const news = newspapers.map(newspaper => axios.get(newspaper.address))
+    const news = newspapers.map((newspaper) => axios.get(newspaper.address))
+    articles = []
 
     //Using promise.all is a way to wait for all the http requests to the publications to either ba fulfilled or one of them to be rejected. This array of results/result is what is returned with this call to promise.all().
     //The response contains a value of the publication pages html gotten by the call to axios.get above.
-    await Promise.all(news) 
-        .then(response => {
-            response.forEach(res => {
+    await Promise.all(news)
+        .then((response) => {
+            response.forEach((res) => {
                 const html = res.data
-                const base = ((new URL(res.config.url)).hostname) //Create a base URL (hostname = domain name response.config.url is an axios url object)
+                const base = new URL(res.config.url).hostname //Create a base URL (hostname = domain name response.config.url is an axios url object)
                 // const name = base.replace('www.', '').split('.')
 
                 //Give the returned response to cheerio to help with picking out what is needed
                 const $ = cheerio.load(html)
 
-                $('div[class="article-body"]>a[href*="football"],div[class^="fc"] a[href*="football"],section[class*="large-col"] a[href*="football"],div[class*="article-list"]>a[href*="football"],h2[class*="list-headline"]>a[href*="football"]', html).each(function () {
+                $(
+                    'div[class="article-body"]>a[href*="football"],div[class^="fc"] a[href*="football"],section[class*="large-col"] a[href*="football"],div[class*="article-list"]>a[href*="football"],h2[class*="list-headline"]>a[href*="football"]',
+                    html
+                ).each(function () {
                     const title = $(this).text()
                     const url = $(this).attr('href')
 
                     articles.push({
                         title,
-                        url: (!url.includes(base) ? `https://${base}` : '') + url,
-                        source: (base.replace('www.', '').split('.'))[0],
+                        url:
+                            (!url.includes(base) ? `https://${base}` : '') +
+                            url,
+                        source: base.replace('www.', '').split('.')[0],
                     })
                 })
             })
         })
-        .catch(error => console.log(error))
+        .catch((error) => console.log(error))
 
     res.json(articles)
 })
@@ -76,18 +82,25 @@ app.get('/footballnews/:newspaperId', (req, res) => {
     const newspaperId = req.params.newspaperId
 
     //Get an array with one element of the specific newspaper object from the newspapers array
-    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
-    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
-    
+    const newspaperAddress = newspapers.filter(
+        (newspaper) => newspaper.name == newspaperId
+    )[0].address
+    const newspaperBase = newspapers.filter(
+        (newspaper) => newspaper.name == newspaperId
+    )[0].base
 
-    //Get the articles from the specific newspaper only and pass them to cheerio 
-    axios.get(newspaperAddress)
-        .then(response => {
+    //Get the articles from the specific newspaper only and pass them to cheerio
+    axios
+        .get(newspaperAddress)
+        .then((response) => {
             const html = response.data
             const $ = cheerio.load(html)
             const specificArticles = []
 
-            $('div[class="article-body"]>a[href*="football"],div[class^="fc"] a[href*="football"],section[class*="large-col"] a[href*="football"],div[class*="article-list"]>a[href*="football"],h2[class*="list-headline"]>a[href*="football"]', html).each(function () {
+            $(
+                'div[class="article-body"]>a[href*="football"],div[class^="fc"] a[href*="football"],section[class*="large-col"] a[href*="football"],div[class*="article-list"]>a[href*="football"],h2[class*="list-headline"]>a[href*="football"]',
+                html
+            ).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
 
